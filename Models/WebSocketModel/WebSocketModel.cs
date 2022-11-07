1a539ac457a040f25873decb1742422f47d7c946
YoshiB33 <94956384+YoshiB33@users.noreply.github.com>
2022-11-07 10:45:15 +0100

    Successfully formated console logs and status.

--- Models/WebSocketModel/WebSocketModel.cs
+++ Models/WebSocketModel/WebSocketModel.cs
@@ -1,10 +1,10 @@
 ﻿namespace Pterodactyl_app.Models.WebSocketModel;
 
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-#pragma warning disable IDE1006 // Naming Styles
+
 public class Rootobject
 {
-    public string _event
+    public string @event
     {
         get; set;
     }
@@ -13,5 +13,5 @@ public class Rootobject
         get; set;
     }
 }
-#pragma warning restore IDE1006 // Naming 
+
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
--- Views/ServerPage.xaml.cs
+++ Views/ServerPage.xaml.cs
@@ -6,10 +6,8 @@ using Pterodactyl_app.Services;
 using Pterodactyl_app.ViewModels;
 using Pterodactyl_app.core.Helpers;
 using Pterodactyl_app.Models.AuthenticateWebsocket;
-using System.Net.Sockets;
 using System.Text;
 using Microsoft.UI.Xaml.Media;
-using Windows.UI;
 using Microsoft.UI;
 
 namespace Pterodactyl_app.Views;
@@ -26,15 +24,12 @@ public sealed partial class ServerPage : Page
         ViewModel = App.GetService<ServerViewModel>();
         InitializeComponent();
     }
-    public static Stream GenerateStreamFromString(string s)
-    {
-        var stream = new MemoryStream();
-        var writer = new StreamWriter(stream);
-        writer.Write(s);
-        writer.Flush();
-        stream.Position = 0;
-        return stream;
-    }
+
+    //readonly JsonSerializerOptions DeserializeOptions = new()
+    //{
+    //    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    //    PropertyNameCaseInsensitive = true,
+    //};
 
     private async void AfterData(Models.ListAPIModel.Attributes data)
     {
@@ -82,7 +77,68 @@ public sealed partial class ServerPage : Page
                             using var reader = new StreamReader(ms, Encoding.UTF8);
                             convertBuffer = await reader.ReadToEndAsync();
                             var FormatedMessage = JsonSerializer.Deserialize<Models.WebSocketModel.Rootobject>(convertBuffer);
-                            LogBox.Text += convertBuffer;
+                            if (FormatedMessage is not null)
+                            {
+                                switch (FormatedMessage.@event)
+                                {
+                                    case "status":
+                                        if (FormatedMessage.args.Contains("offline"))
+                                        {
+                                            StatusBox.Text = "Offline";
+                                            StatusBox.Foreground = new SolidColorBrush(Colors.Red);
+                                        }
+                                        else if (FormatedMessage.args.Contains("running"))
+                                        {
+                                            StatusBox.Text = "Online";
+                                            StatusBox.Foreground = new SolidColorBrush(Colors.Green);
+                                        }
+                                        else if (FormatedMessage.args.Contains("starting"))
+                                        {
+                                            StatusBox.Text = "Starting";
+                                            StatusBox.Foreground = new SolidColorBrush(Colors.Yellow);
+                                        }
+                                        else if (FormatedMessage.args.Contains("stopping"))
+                                        {
+                                            StatusBox.Text = "Stopping";
+                                            StatusBox.Foreground = new SolidColorBrush(Colors.Orange);
+                                        }
+                                        break;
+                                    case "console output":
+                                        foreach (var item in FormatedMessage.args)
+                                        {
+                                            if (item.EndsWith("[m"))
+                                            {
+                                                LogBox.Text += $"{item.Substring(0, item.Length - 2)}\n";
+                                            }
+                                            else
+                                            {
+                                                LogBox.Text += $"{item}\n";
+                                            }
+                                        }
+                                        break;
+                                    case "token expiring":
+                                        var newApiToWebsocket = await APIService.Client.GetAsync($"https://{APIService.GetServerURL()}/api/client/servers/{data.uuid}/websocket");
+                                        if (newApiToWebsocket.IsSuccessStatusCode)
+                                        {
+                                            var newWebsocketCredentials = await JsonSerializer.DeserializeAsync<Models.EstablishWebsocket.Rootobject>(apiToWebsocket.Content.ReadAsStream());
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+                                            await Websocket.Send(Socket, newWebsocketCredentials.data.token);
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+                                        }
+                                        break;
+                                    case "token expired":
+                                        AfterData(data);
+                                        break;
+                                    case "auth success":
+                                        LogBox.Text += "Successfully connected!\n";
+                                        break;
+                                    case "stats":
+                                        break;
+                                    default:
+                                        LogBox.Text += $"No match {FormatedMessage.@event}\n";
+                                        break;
+                                }
+                            }
                         } while (true);
                     } while (true);
                 }
